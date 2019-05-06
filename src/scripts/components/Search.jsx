62c4934fdd86f8c935a1fb2c2cd59bab1988f82b
Jean Rauwers <jeanrauwers@gmail.com>
2019-05-06 15:24:01 +0100

    Reverting refactoring to keep the code cleaner

--- src/scripts/components/Search.jsx
+++ src/scripts/components/Search.jsx
@@ -1,46 +1,37 @@
 import React, { useState } from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-const searchQueryHandler = (evt, search, searchValue, setInputIsValid) => {
-	evt.preventDefault();
-	if (!searchValue.replace(/\s/, '').length) {
-		messageValidator(setInputIsValid);
-		return;
-	}
-
-	search(searchValue.toString());
-	resetInputField(setInputIsValid);
-};
-
-const messageValidator = setInputIsValid => {
-	setInputIsValid(false);
-	setTimeout(() => {
-		setInputIsValid(true);
-	}, 2000);
-};
-
-const resetInputField = setSearchValue => {
-	setSearchValue('');
-};
-
 const Search = ({ search }) => {
 	const [searchValue, setSearchValue] = useState('');
 	const [inputIsValid, setInputIsValid] = useState(true);
-
 	const inputValueHandler = evt => {
 		setSearchValue(evt.target.value);
+	};
+	const resetInputField = () => {
+		setSearchValue('');
+	};
+	const searchQueryHandler = evt => {
+		evt.preventDefault();
+		if (!searchValue.replace(/\s/, '').length) {
+			messageValidator();
+			return;
+		}
+
+		search(searchValue.toString());
+		resetInputField();
+	};
+
+	const messageValidator = () => {
+		setInputIsValid(false);
+		setTimeout(() => {
+			setInputIsValid(true);
+		}, 2000);
 	};
 	return (
 		<>
 			<Form className="search-form">
 				<Input value={searchValue} onChange={inputValueHandler} type="text" />
-				<Input
-					onClick={evt =>
-						searchQueryHandler(evt, search, searchValue, setInputIsValid)
-					}
-					type="submit"
-					value="SEARCH"
-				/>
+				<Input onClick={searchQueryHandler} type="submit" value="SEARCH" />
 			</Form>
 		</>
 	);
