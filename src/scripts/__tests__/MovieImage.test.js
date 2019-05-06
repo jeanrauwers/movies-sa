e21f6b3a7c3e4851ec8ffe9a20636ab51716a4b8
Jean Rauwers <jeanrauwers@gmail.com>
2019-05-06 14:24:42 +0100

    Adding MovieTitle test and improving description for MovieImage

--- src/scripts/__tests__/MovieImage.test.js
+++ src/scripts/__tests__/MovieImage.test.js
@@ -8,12 +8,12 @@ describe('Testing MovieImage component', () => {
 		backdrop_path: 'test'
 	};
 	const movieImage = shallow(<MovieImage movie={movieObj} />);
-	test('should have alt tag', () => {
+	test('should have alt tag to be iquals to The movie title: ${movieObj.title}', () => {
 		expect(movieImage.find('img').prop('alt')).toEqual(
 			`The movie title: ${movieObj.title}`
 		);
 	});
-	test('Header must have h2 value of test', () => {
+	test('Header must have src value of https://image.tmdb.org/t/p/w500${movieObj.backdrop_path}', () => {
 		expect(movieImage.find('img').prop('src')).toEqual(
 			`https://image.tmdb.org/t/p/w500${movieObj.backdrop_path}`
 		);
